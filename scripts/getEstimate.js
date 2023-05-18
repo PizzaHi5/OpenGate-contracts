bde8d46621ad329d454255046cf8b988722cc8f8
caixiangfan <caixiang@ualberta.ca>
2023-05-18 23:26:26 +0000

    make getEstimate.js work

--- scripts/getEstimate.js
+++ scripts/getEstimate.js
@@ -1,21 +1,37 @@
-//const { ethers } = require("ethers");
-//const hre = require("@nomicfoundation/hardhat-foundry");
-
-
-const hree = require("hardhat");
-require('dotenv').config();
+const { ethers } = require("ethers");
+const fs = require("fs");
 const DONconsumerABI = require('../out/DONconsumer.sol/DONconsumer.json');
+require('dotenv').config();
 
+const main = async() => {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.MUMBAI_RPC_URL);
+  const signer = provider.getSigner();
+  const requestConfig = {
+    source: fs.readFileSync("./calculation-example.js").toString(),
+    args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
+    secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? "" },
+    subId: 655,
+    gasLimit: 1000000,
+    gasPrice: await (await provider.getFeeData()).gasPrice
+  }
+  const DONconsumerAddress = process.env.DON_CONSUMER_CONTRACT ?? ''; 
+  console.log({DONconsumerAddress})
+  const consumerContract = new ethers.Contract(DONconsumerAddress, DONconsumerABI.abi, provider);
+  const simulatedSecretsURLBytes = `0x${Buffer.from(
+    "https://exampleSecretsURL.com/f09fa0db8d1c8fab8861ec97b1d7fdf1/raw/d49bbd20dc562f035bdf8832399886baefa970c9/encrypted-functions-request-data-1679941580875.json"
+  ).toString("hex")}`;
 
-module.exports = async() => {
-    const provider = new hree.ethers.providers.JsonRpcProvider(process.env.MUMBAI_API_KEY);
-    const signer = hree.provider.getSigner();
+  const estimatedCost = await consumerContract.getCostEstimate(
+    requestConfig.source,
+    requestConfig.secrets && Object.keys(requestConfig.secrets).length > 0 ? simulatedSecretsURLBytes : [],
+    requestConfig.args ?? [],
+    requestConfig.subId,
+    requestConfig.gasLimit,
+    requestConfig.gasPrice
+  );
 
-    const number = await hree.provider.getBlockNumber(); 
-    console.log(number);
+  console.log({estimatedCost});
+  console.log(ethers.utils.formatEther(estimatedCost));
+} 
 
-    //const DONaddress = await 
-
-}
-
-module.exports.tags = ['getEstimate'];
+main()
