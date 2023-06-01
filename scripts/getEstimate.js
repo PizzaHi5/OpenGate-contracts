06a83787418a1e2b22fa767c1bd0ee2938494cee
caixiangfan <caixiang@ualberta.ca>
2023-06-01 19:32:07 +0000

    tried executeRequest

--- scripts/getEstimate.js
+++ scripts/getEstimate.js
@@ -5,21 +5,18 @@ require('dotenv').config();
 
 const main = async() => {
   const provider = new ethers.providers.JsonRpcProvider(process.env.MUMBAI_RPC_URL);
-  const signer = provider.getSigner();
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
   const requestConfig = {
     source: fs.readFileSync("./calculation-example.js").toString(),
     args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
     secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? "" },
-    subId: 655,
-    gasLimit: 1000000,
-    gasPrice: await (await provider.getFeeData()).gasPrice
+    subId: process.env.SUB_ID ?? 0,
+    gasLimit: process.env.GAS_LIMIT ?? 1000000,
+    gasPrice: (await provider.getFeeData()).gasPrice
   }
   const DONconsumerAddress = process.env.DON_CONSUMER_CONTRACT ?? ''; 
-  console.log({DONconsumerAddress})
-  const consumerContract = new ethers.Contract(DONconsumerAddress, DONconsumerABI.abi, provider);
-  const simulatedSecretsURLBytes = `0x${Buffer.from(
-    "https://exampleSecretsURL.com/f09fa0db8d1c8fab8861ec97b1d7fdf1/raw/d49bbd20dc562f035bdf8832399886baefa970c9/encrypted-functions-request-data-1679941580875.json"
-  ).toString("hex")}`;
+  const consumerContract = new ethers.Contract(DONconsumerAddress, DONconsumerABI.abi, signer);
+  const simulatedSecretsURLBytes = `0x${Buffer.from(process.env.BUFFER_URL ?? "").toString("hex")}`;
 
   const estimatedCost = await consumerContract.getCostEstimate(
     requestConfig.source,
@@ -29,9 +26,22 @@ const main = async() => {
     requestConfig.gasLimit,
     requestConfig.gasPrice
   );
+  console.log("estimatedCost: ", estimatedCost.toNumber()/1e8);
 
-  console.log({estimatedCost});
-  console.log(ethers.utils.formatEther(estimatedCost));
+  const overrides = {
+    gasLimit: 1_500_000
+  }
+
+  const executeRes = await consumerContract.executeRequest(
+    requestConfig.source,
+    requestConfig.secrets && Object.keys(requestConfig.secrets).length > 0 ? simulatedSecretsURLBytes : [],
+    requestConfig.args ?? [],
+    requestConfig.subId,
+    requestConfig.gasLimit,
+    overrides
+  );
+
+  console.log({executeRes});
 } 
 
 main()
